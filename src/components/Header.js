c1fe38154a62720bc22f99425d86efaaa790b331
Jay Cui <seryu.lucky@gmail.com>
2022-06-01 08:41:41 -0600

    Created the app

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavItem from './NavItem'
+
+export default function Header() {
+    return (
+        <>
+            <div>Header</div>
+            <NavItem />
+            <NavItem />
+            <NavItem />
+            <NavItem />
+            <NavItem />
+            <NavItem />
+            <NavItem />
+        </>
+    )
+}
