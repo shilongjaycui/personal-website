5ca23d443b4e994f87f61f6513f9d531b949c668
Jay Cui <seryu.lucky@gmail.com>
2022-06-03 08:10:50 -0600

    Installed Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="text-center">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="opacity-50 h-96" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code className='text-black' >src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="italic"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
