785cd32d947be8ea1a1ff51303d656225ac8b9ab
Jay Cui <shilong@byu.edu>
2022-06-04 10:44:28 -0600

    Designed the name heading

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Header() {
     return (
-        <div className="relative p-4 h-40 bg-red-800 shadow-2xl	shadow-black opacity-95">
-            <h1 className="absolute inset-y-1/2 w-56 h-16 font-piazzolla">Jay Cui</h1>
+        <div className="flex items-center p-4 h-40 bg-red-800 shadow-2xl shadow-black opacity-95">
+            <h1 className="w-56 h-16 font-piazzolla text-white text-6xl font-bold">Jay Cui</h1>
         </div>
     )
 }
