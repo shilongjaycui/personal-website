18272f5d536f29cb5474e860eb18dca3c1abe240
Jay Cui <shilong@byu.edu>
2022-06-04 12:50:38 -0600

    WIP

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import stroke from '../images/home/stroke.png'  // downloaded from https://flyclipart.com/brush-stroke-transparent-background-white-brush-stroke-png-465374#
 
 export default function NavItem(props) {
     return (
-        <div className="w-56 h-10 font-piazzolla text-white text-3xl align-center">{props.label}</div>
+        <div className="w-56 h-10 font-piazzolla text-white text-3xl align-center bg-stroke">{props.label}</div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
     extend: {
       fontFamily: {
         'piazzolla': ['"Piazzolla"', 'serif']
+      },
+      backgroundImage: {
+        'stroke': "url('/src/images/home/stroke.png')",
       }
     },
   },
