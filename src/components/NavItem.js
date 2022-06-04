047164df97e5663a6b1ef625e5e856ee18fb2647
Jay Cui <shilong@byu.edu>
2022-06-04 14:50:36 -0600

    Rendered navigation item background

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import stroke from '../images/home/stroke.png'  // downloaded from https://flyclipart.com/brush-stroke-transparent-background-white-brush-stroke-png-465374#
 
 export default function NavItem(props) {
     return (
-        <div className="w-56 h-10 font-piazzolla text-white text-3xl align-center bg-stroke">{props.label}</div>
+        <div className="flex justify-center items-center w-56 h-24 font-piazzolla text-center text-white text-3xl hover:bg-stroke bg-local">{props.label}</div>
     )
 }
