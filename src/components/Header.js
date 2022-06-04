953ef852377d796471e0537b26f67501cccbcfce
Jay Cui <shilong@byu.edu>
2022-06-04 09:47:43 -0600

    Designed header background

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div className="relative p-4 h-40 bg-red-800 shadow-2xl	shadow-black opacity-95">
+            This is your personal website's header.
+        </div>
+    )
+}
