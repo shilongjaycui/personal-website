a51ae77a7e6ac67a7b649404ecdc72bf6083a20f
Jay Cui <seryu.lucky@gmail.com>
2022-06-06 17:18:54 -0600

    Created the About route

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,15 @@ import React from 'react';
 import CollapsibleNavItem from './CollapsibleNavItem';
 import NavItem from './NavItem';
 import resume from '../documents/jay_cui_spring_2022.pdf';
+import { Link } from "react-router-dom";
 
 export default function Header() {
     return (
         <div className="flex items-center p-4 h-40 bg-red-800 shadow-2xl shadow-black opacity-95">
             <h1 className="w-64 h-16 font-piazzolla text-center text-white text-6xl font-bold hover:uppercase">Jay Cui</h1>
-            <NavItem label='About'/>
+            <Link to="/about">
+                <NavItem label='About'/>
+            </Link>
             <NavItem label='Blog' url='https://medium.com/@shilongjaycui'/>
             <CollapsibleNavItem label='Projects' items={['Music', 'Dating', 'Reading']}/>
             <NavItem label='LinkedIn' url='https://www.linkedin.com/in/shilongjaycui/'/>
