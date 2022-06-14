720ecdbf1edd018c12a898bf2de05cb69556684e
Jay Cui <seryu.lucky@gmail.com>
2022-06-14 17:23:07 -0600

    Added routing back to homepage

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -4,9 +4,9 @@ export default function NavItem(props) {
     return (
         <a 
             className="flex justify-center items-center 
-                w-56 h-24 
+                w-48 h-24 
                 font-piazzolla text-white text-3xl 
-                hover:bg-stroke bg-local" 
+                hover:bg-stroke hover:bg-auto" 
             href={props.url}
         >
             {props.label}
