d69638bc87382b7dc44adef0399925662a513166
Jay Cui <shilong@byu.edu>
2022-06-18 18:02:13 -0600

    Implemented NavMenu for both laptops and smartphones

--- src/App.js
+++ src/App.js
@@ -1,43 +1,46 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import Header from "./components/Header";
-import NavItem from "./components/NavItem";
-import CollapsibleNavItem from './components/CollapsibleNavItem';
+import NavMenu from "./components/NavMenu";
 import list from "./images/home/list.png";
 import cyborg_commando from "./images/home/cyborg_commando.png";
-import resume from './documents/jay_cui_spring_2022.pdf';
 
 function App() {
+  const MACBOOK_MIN_WIDTH = 1200
+  const [showNavMenu, setShowNavMenu] = useState(window.innerWidth >= MACBOOK_MIN_WIDTH)
+  const [listIconClicked, setListIconClicked] = useState(false)
+
+  useEffect(() => {
+    function handleResize() {
+      setShowNavMenu((window.innerWidth >= MACBOOK_MIN_WIDTH) || listIconClicked)
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    handleResize()
+
+    return () => window.removeEventListener('resize', handleResize)
+  })
+
   return (
     <div className="w-screen h-screen">
       <div className="bg-red-800 w-full h-1/6 flex flex-row relative shadow-2xl shadow-black">
         <img
           src={list}
           alt="list"
-          className="MacBook:h-0 iPad:h-1/3 iPhone:h-1/3 absolute left-10 flex ml-auto inset-y-1/3"
+          className="MacBook:h-0 iPad:h-1/3 iPhone:h-1/3 absolute left-10 flex ml-auto inset-y-1/3 cursor-pointer"
+          onClick={() => setListIconClicked((prevState) => {
+            return !prevState
+          })}
         />
-        <Link to="/" className="w-full h-full flex justify-center items-center">
-          <h1 className="font-piazzolla text-white text-6xl font-bold hover:text-black">
+        <Link to="/" className="w-full h-full flex justify-center items-center cursor-auto">
+          <h1 className="font-piazzolla text-white text-6xl font-bold hover:text-black cursor-pointer">
             Jay Cui
           </h1>
         </Link>
       </div>
       <div className="relative h-5/6">
-        <div className="absolute bg-red-800 
-          MacBook:w-full MacBook:h-1/6 iPad:w-1/2 iPad:h-full iPhone:w-1/2 iPhone:h-full 
-          z-10 flex justify-center items-center
-          MacBook:flex-row iPad:flex-col iPhone:flex-col"
-        >
-          <Link to="/about">
-            <NavItem label='About'/>
-          </Link>
-          <NavItem label='Blog' url='https://medium.com/@shilongjaycui'/>
-          <CollapsibleNavItem label='Projects' items={['Music', 'Dating', 'Reading']}/>
-          <NavItem label='LinkedIn' url='https://www.linkedin.com/in/shilongjaycui/'/>
-          <NavItem label='GitHub' url='https://github.com/shilongjaycui'/>
-          <NavItem label='Resume' url={resume}/>
-        </div>
+        {showNavMenu && (<NavMenu />)}
         <img src={cyborg_commando} alt={"大佬"} className="absolute z-0" />
       </div>
     </div>
