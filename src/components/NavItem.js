cdc1755c00e17cfade472957a2087e5be033be5e
Jay Cui <shilong@byu.edu>
2022-06-19 13:46:34 -0600

    Moved showProjects state one child component down

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React from "react";
 
 export default function NavItem(props) {
-    return (
-        <a 
-            className="flex justify-center items-center 
+  return (
+    <a
+      className="flex justify-center items-center 
                 w-36 h-16
                 m-2
                 font-piazzolla text-white text-3xl 
-                hover:bg-stroke hover:bg-contain" 
-            href={props.url}
-        >
-            {props.label}
-        </a>
-    )
+                hover:bg-stroke hover:bg-contain hover:cursor-pointer"
+      href={props.url}
+      onClick={() => {
+        if (props.updateShowProjects) {
+          props.updateShowProjects();
+        }
+      }}
+    >
+      {props.label}
+    </a>
+  );
 }
--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,32 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import NavItem from "./NavItem";
-import CollapsibleNavItem from "./CollapsibleNavItem";
 import resume from "../documents/jay_cui_spring_2022.pdf";
 
-export default function NavMenu({ updateShowProjects }) {
+export default function NavMenu() {
+  const [showProjects, setShowProjects] = useState(false)
+  const updateShowProjects = () => {
+    setShowProjects((prevState) => {
+      console.log('showProjects (before):', prevState)
+      console.log('showProjects (after):', !prevState)
+      return !prevState
+    })
+  }
   return (
-    <div
-      className="absolute bg-red-800 opacity-75
-            MacBook:w-full MacBook:h-1/6 iPad:w-1/2 iPad:h-full iPhone:w-1/2 iPhone:h-full 
-            z-10 flex justify-center items-center
-            MacBook:flex-row iPad:flex-col iPhone:flex-col"
-    >
-      <Link to="/about">
-        <NavItem label="About" />
-      </Link>
-      <NavItem label="Blog" url="https://medium.com/@shilongjaycui" />
-      <CollapsibleNavItem
-        label="Projects"
-        items={["Music", "Dating", "Reading"]}
-        updateShowProjects={updateShowProjects}
-      />
-      <NavItem
-        label="LinkedIn"
-        url="https://www.linkedin.com/in/shilongjaycui/"
-      />
-      <NavItem label="GitHub" url="https://github.com/shilongjaycui" />
-      <NavItem label="Resume" url={resume} />
-    </div>
+    <>
+      <div
+        className="absolute bg-red-800 opacity-75
+              MacBook:w-full MacBook:h-1/6 iPad:w-1/2 iPad:h-full iPhone:w-1/2 iPhone:h-full 
+              z-10 flex justify-center items-center
+              MacBook:flex-row iPad:flex-col iPhone:flex-col"
+      >
+        <Link to="/about">
+          <NavItem label="About" />
+        </Link>
+        <NavItem label="Blog" url="https://medium.com/@shilongjaycui" />
+        <NavItem
+          label="Projects"
+          items={["Music", "Dating"]}
+          updateShowProjects={updateShowProjects}
+        />
+        <NavItem
+          label="LinkedIn"
+          url="https://www.linkedin.com/in/shilongjaycui/"
+        />
+        <NavItem label="GitHub" url="https://github.com/shilongjaycui" />
+        <NavItem label="Resume" url={resume} />
+      </div>
+      {showProjects && (
+        <div className="static w-full h-1/4 bg-white z-20"></div>
+      )}
+    </>
   );
 }
