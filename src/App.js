1574f334ed5ca57dd04d87643b8987cbeedf25ad
Jay Cui <shilong@byu.edu>
2022-06-19 18:21:33 -0600

    Fixed mobile view on homepage and About page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header"
-import cyborg_commando from "./images/home/cyborg_commando.png";
 
 function App() {
   return (
     <div className="relative w-screen h-screen">
       <Header />
-      <img src={cyborg_commando} alt={"大佬"} className="absolute z-0" />
+      <div className="absolute z-0 w-full h-full
+                      bg-cyborg_commando bg-top bg-cover"
+      ></div>
     </div>
   );
 }
--- src/routes/about.js
+++ src/routes/about.js
@@ -1,32 +1,37 @@
-import StoryWindow from '../components/StoryWindow';
-import Korea from '../images/about/south_korea.png';
-import Japan from '../images/about/japan.png';
-import China from '../images/about/china.png';
-import US from '../images/about/US.png';
-import BYU from '../images/about/BYU.png';
-import markov_chain from '../images/about/markov_chain.png';
-import react from '../images/about/react.png';
-import calisthenics from '../images/about/calisthenics.png';
-import accordion from '../images/about/accordion.png';
+import Header from "../components/Header";
+import StoryWindow from "../components/StoryWindow";
+import Korea from "../images/about/south_korea.png";
+import Japan from "../images/about/japan.png";
+import China from "../images/about/china.png";
+import US from "../images/about/US.png";
+import BYU from "../images/about/BYU.png";
+import markov_chain from "../images/about/markov_chain.png";
+import react from "../images/about/react.png";
+import calisthenics from "../images/about/calisthenics.png";
+import accordion from "../images/about/accordion.png";
 
 export default function About() {
-    return (
-        <div className='bg-sky-800 h-full'>
-            <div className='flex flex-row justify-center items-baseline'>
-                <StoryWindow src={Korea} label='ethnic Korean'/>
-                <StoryWindow src={Japan} label='born in Japan'/>
-                <StoryWindow src={China} label='Chinese citizen'/>
-                <StoryWindow src={US} label='living in the US'/>
-            </div>
-            <div className='flex flex-row justify-center items-baseline'>
-                <StoryWindow src={BYU} label='undergraduate student'/>
-                <StoryWindow src={markov_chain} label='applied mathematician'/>
-                <StoryWindow src={react} label='software engineer'/>
-            </div>
-            <div className='flex flex-row justify-center items-baseline'>
-                <StoryWindow src={calisthenics} label='calisthenics athlete'/>
-                <StoryWindow src={accordion} label='accordionist'/>
-            </div>
+  return (
+    <div className="relative w-screen h-screen">
+      <Header />
+      <div className="bg-amber-400 h-1/6"></div>
+      <div className="bg-amber-400 MacBook:h-fit iPad:h-screen iPhone:h-screen">
+        <div className="flex flex-row justify-center items-baseline">
+          <StoryWindow src={Korea} label="ethnic Korean" />
+          <StoryWindow src={Japan} label="born in Japan" />
+          <StoryWindow src={China} label="Chinese citizen" />
+          <StoryWindow src={US} label="living in the US" />
         </div>
-    );
+        <div className="flex flex-row justify-center items-baseline">
+          <StoryWindow src={BYU} label="undergraduate student" />
+          <StoryWindow src={markov_chain} label="applied mathematician" />
+          <StoryWindow src={react} label="software engineer" />
+        </div>
+        <div className="flex flex-row justify-center items-baseline">
+          <StoryWindow src={calisthenics} label="calisthenics athlete" />
+          <StoryWindow src={accordion} label="accordionist" />
+        </div>
+      </div>
+    </div>
+  );
 }
