455577622ac71d673fc9dd396a5c37e28887b8ae
Jay Cui <shilong@byu.edu>
2022-06-20 19:42:22 -0600

    Hid NavMenu on About page

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import list from "../images/home/list.png";
 import { Link } from "react-router-dom";
 import NavMenu from "./NavMenu";
 
-export default function Header() {
+export default function Header(props) {
   const MACBOOK_MIN_WIDTH = 1200;
   const [showNavMenu, setShowNavMenu] = useState(
     window.innerWidth >= MACBOOK_MIN_WIDTH
@@ -27,25 +27,28 @@ export default function Header() {
   return (
     <>
       <div className="bg-red-800 w-full h-1/6 flex flex-row relative shadow-2xl shadow-black">
-        <img
-          src={list}
-          alt="list"
-          className="MacBook:h-0 iPad:h-1/3 iPhone:h-1/3 absolute left-10 flex ml-auto inset-y-1/3 cursor-pointer"
-          onClick={() =>
-            setListIconClicked((prevState) => {
-              return !prevState;
-            })
-          }
-        />
-        <div
-          className="w-full h-full flex justify-center items-center"
-        >
-          <Link to="/" className="font-piazzolla text-white text-6xl font-bold hover:text-black cursor-pointer">
+        {!props.hideNavMenu && (
+          <img
+            src={list}
+            alt="list"
+            className="MacBook:h-0 iPad:h-1/3 iPhone:h-1/3 absolute left-10 flex ml-auto inset-y-1/3 cursor-pointer"
+            onClick={() =>
+              setListIconClicked((prevState) => {
+                return !prevState;
+              })
+            }
+          />
+        )}
+        <div className="w-full h-full flex justify-center items-center">
+          <Link
+            to="/"
+            className="font-piazzolla text-white text-6xl font-bold hover:text-black cursor-pointer"
+          >
             Jay Cui
           </Link>
         </div>
       </div>
-      {showNavMenu && <NavMenu />}
+      {!props.hideNavMenu && showNavMenu && <NavMenu />}
     </>
   );
 }
