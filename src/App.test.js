a16ad5b7b3e7959cee43ff79197d46c1e72010d8
Jay Cui <shilong@byu.edu>
2023-03-05 13:24:04 -0700

    WIP

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import { Router } from "react-router-dom";
-
-test('renders my name', () => {
-  const name = 'jay'
-  expect(name).toBe('jay')
-});
