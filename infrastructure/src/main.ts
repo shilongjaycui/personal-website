a1e76ae6d3c5ed0d2af28f358c36e058fb980d17
Jay Cui <shilongjaycui@gmail.com>
2023-11-02 18:18:41 -0600

    Got deployment to work again

--- infrastructure/src/main.ts
+++ infrastructure/src/main.ts
@@ -1,120 +1,101 @@
-import { App, Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
+import { App, Stack, StackProps, RemovalPolicy, Duration } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
-import * as s3_deployment from 'aws-cdk-lib/aws-s3-deployment';
 import * as path from 'path';
-import { HttpMethods } from 'aws-cdk-lib/aws-s3';
 import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as route53targets from 'aws-cdk-lib/aws-route53-targets';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 
 const PERSONAL_WEBSITE_DOMAIN = "shilongjaycui.com"
 
 export interface PersonalWebsiteProps extends StackProps {
   path: string;
-  cloudfrontCertificationArn?: string;
+  cloudfrontCertificationArn: string;
 }
 
 export class PersonalWebsiteStack extends Stack {
   constructor(scope: Construct, id: string, props: PersonalWebsiteProps) {
     super(scope, id, props);
 
-    const { path } = props;
-
     const subdomainBucket = new s3.Bucket(this, 'subdomain-bucket', {
       bucketName: `www.${PERSONAL_WEBSITE_DOMAIN}`,
       publicReadAccess: false,  // no public access, user must access via cloudfront
       removalPolicy: RemovalPolicy.DESTROY,
+      accessControl: s3.BucketAccessControl.PRIVATE,
+      objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_ENFORCED,
+      encryption: s3.BucketEncryption.S3_MANAGED,
       autoDeleteObjects: true,
-      cors: [
-        {
-          allowedHeaders: ["*"],
-          allowedMethods: [HttpMethods.GET],
-          allowedOrigins: ["*"],
-          exposedHeaders: [],
-        },
-      ],
-      websiteIndexDocument: 'index.html',
-      websiteErrorDocument: '404.html',
     });
-    new s3.Bucket(this, 'root-domain-bucket', {
-      bucketName: PERSONAL_WEBSITE_DOMAIN,
-      publicReadAccess: false,
-      removalPolicy: RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-      websiteRedirect: {
-        hostName: `www.${PERSONAL_WEBSITE_DOMAIN}`,
-        protocol: s3.RedirectProtocol.HTTPS,
-      },
-    });
-    const originAccessIdentity = new cloudfront.OriginAccessIdentity(this, "bucket-origin-access-identity")
-    const personalWebsitePolicyStatement = new iam.PolicyStatement(
+    const originAccessIdentity = new cloudfront.OriginAccessIdentity(this, "origin-access-identity")
+    const iamPolicyStatement = new iam.PolicyStatement(
       {
         actions: ['s3:GetObject'],
         resources: [subdomainBucket.arnForObjects("*")],
         principals: [
-          new iam.CanonicalUserPrincipal(
-            originAccessIdentity.cloudFrontOriginAccessIdentityS3CanonicalUserId
-          ),
+          new iam.CanonicalUserPrincipal(originAccessIdentity.cloudFrontOriginAccessIdentityS3CanonicalUserId),
         ],
       },
     );
-    subdomainBucket.addToResourcePolicy(personalWebsitePolicyStatement);
+    subdomainBucket.addToResourcePolicy(iamPolicyStatement);
+    
+    // The Route53 hosted zone and the ACM certificate were created manually beforehand because it's hard to manage them with a CloudFormation stack.
+    const hostedZone = route53.HostedZone.fromLookup(this, "existing-hosted-zone", {
+      domainName: `${PERSONAL_WEBSITE_DOMAIN}.`,
+    });
+    const certificate = acm.Certificate.fromCertificateArn(this, "existing-certificate", props.cloudfrontCertificationArn);
 
-    const personalWebsiteDistribution = new cloudfront.CloudFrontWebDistribution(this, "cloudfront-web-distribution", {
-      originConfigs: [
-        {
-          s3OriginSource: {
-            s3BucketSource: subdomainBucket,
-            originAccessIdentity: originAccessIdentity,
-          },
-          behaviors: [
-            {
-              viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-              allowedMethods: cloudfront.CloudFrontAllowedMethods.GET_HEAD,
-              compress: true,
-              isDefaultBehavior: true,
-            },
-          ],
+    const cloudfrontResponseHeaderPolicy = new cloudfront.ResponseHeadersPolicy(this, 'SecurityHeadersResponseHeaderPolicy', {
+      comment: 'Security headers response header policy',
+      securityHeadersBehavior: {
+        contentSecurityPolicy: {
+          override: true,
+          contentSecurityPolicy: "default-src 'self'"
         },
-      ],
-      viewerCertificate: {
-        aliases: [
-          PERSONAL_WEBSITE_DOMAIN,
-          `www.${PERSONAL_WEBSITE_DOMAIN}`,
-        ],
-        props: {
-          acmCertificateArn: props.cloudfrontCertificationArn,
-          minimumProtocolVersion: "TLSv1.2_2021",
-          sslSupportMethod: "sni-only",
-        }
+        strictTransportSecurity: {
+          override: true,
+          accessControlMaxAge: Duration.days(2 * 365),
+          includeSubdomains: true,
+          preload: true
+        },
+        contentTypeOptions: {
+          override: true
+        },
+        referrerPolicy: {
+          override: true,
+          referrerPolicy: cloudfront.HeadersReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN
+        },
+        xssProtection: {
+          override: true,
+          protection: true,
+          modeBlock: true
+        },
+        frameOptions: {
+          override: true,
+          frameOption: cloudfront.HeadersFrameOption.DENY
+        },
+      }
+    });
+
+    const cloudfrontDistribution = new cloudfront.Distribution(this, 'CloudFrontDistribution', {
+      certificate: certificate,
+      domainNames: [PERSONAL_WEBSITE_DOMAIN],
+      defaultRootObject: 'index.html',
+      defaultBehavior: {
+      origin: new origins.S3Origin(subdomainBucket, {
+          originAccessIdentity: originAccessIdentity
+      }),
+      viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      responseHeadersPolicy: cloudfrontResponseHeaderPolicy
       },
-      defaultRootObject: "index.html",
-      errorConfigurations: [
-        {
-          errorCode: 403,
-          responseCode: 200,
-          responsePagePath: "/index.html",
-        },
-      ],
     });
 
-    new s3_deployment.BucketDeployment(this, "personalWebsiteBucketDeployment", {
-      sources: [s3_deployment.Source.asset(path)],
-      destinationBucket: subdomainBucket,
-      distribution: personalWebsiteDistribution,
-    });
-
-    // Look up the zone based on domain name.
-    const hostedZone = route53.HostedZone.fromLookup(this, "baseZone", {
-      domainName: PERSONAL_WEBSITE_DOMAIN,
-    });
-
-    // Add the subdomain to Route 53
-    new route53.CnameRecord(this, 'test.baseZone', {
-      zone: hostedZone,
-      recordName: `www.${PERSONAL_WEBSITE_DOMAIN}.`,
-      domainName: subdomainBucket.bucketDomainName,
+    new route53.ARecord(this, 'ARecord', {
+        recordName: PERSONAL_WEBSITE_DOMAIN,
+        target: route53.RecordTarget.fromAlias(new route53targets.CloudFrontTarget(cloudfrontDistribution)),
+        zone: hostedZone,
     });
   }
 }
